c0c4ed49292911f1da081e3b14d88e85e6f544ed
Anvar <anvar.kayumov.personal@gmail.com>
2022-04-21 18:51:04 +0500

    Add token price route

--- Sources/CoinGeckoApi/CoinGeckoRoute.swift
+++ Sources/CoinGeckoApi/CoinGeckoRoute.swift
@@ -11,6 +11,17 @@ public enum CoinGeckoRoute: Route {
         include24hrChange: Bool,
         includeLastUpdatedAt: Bool
     )
+    case simpleTokenPrice(
+        id: String,
+        contractAddresses: [String],
+        vsCurrencies: [String],
+        includeMarketCap: Bool,
+        include24hrVol: Bool,
+        include24hrChange: Bool,
+        includeLastUpdatedAt: Bool
+    )
+    case simpleSupportedVsCurrencies
+    
     case coinsList(includePlatform: Bool)
 
     public var path: String {
@@ -24,6 +35,17 @@ public enum CoinGeckoRoute: Route {
             let include24hrChange,
             let includeLastUpdatedAt
         ): return "/simple/price?ids=\(ids.joined(separator: ","))&vs_currencies=\(vsCurrencies.joined(separator: ","))&include_market_cap=\(includeMarketCap)&include_24hr_vol=\(include24hrVol)&include_24hr_change=\(include24hrChange)&include_last_updated_at=\(includeLastUpdatedAt)"
+        case .simpleTokenPrice(
+            let id,
+            let contractAddresses,
+            let vsCurrencies,
+            let includeMarketCap,
+            let include24hrVol,
+            let include24hrChange,
+            let includeLastUpdatedAt
+        ): return "/simple/token_price/\(id)?contract_addresses=\(contractAddresses.joined(separator: ","))&vs_currencies=\(vsCurrencies.joined(separator: ","))&include_market_cap=\(includeMarketCap)&include_24hr_vol=\(include24hrVol)&include_24hr_change=\(include24hrChange)&include_last_updated_at=\(includeLastUpdatedAt)"
+        case .simpleSupportedVsCurrencies: return "/simple/supported_vs_currencies"
+            
         case .coinsList(let includePlatform):
             return "/coins/list?include_platform=\(includePlatform)"
         }
